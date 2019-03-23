aa68a97049aebbd27f0a853af6e4cb423741a1bf
hungnttg <hungnttg@gmail.com>
2019-03-23 15:40:27 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sort
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] mang = new int[5] { 4, 5, 1, 2, -1 };
+            Console.WriteLine("Before Sort: ");
+            for (int l=0;l<5;l++)
+            {
+
+                Console.Write(mang[l] + ",");
+            }
+            //int j = 0;
+            for (int j=0;j<4;j++)
+            {
+                for (int i = j + 1; i >0; i--) 
+                {
+                    if (mang[i] < mang[i - 1])
+                    {
+                        int temp = mang[i - 1];
+                        mang[i - 1] = mang[i];
+                        mang[i] = temp;
+                    }
+                }
+            }
+            //in
+            Console.WriteLine("After Sort");
+            for (int k=0;k<5;k++)
+            {
+
+                Console.Write(mang[k] + ",");
+            }
+        }
+    }
+}
